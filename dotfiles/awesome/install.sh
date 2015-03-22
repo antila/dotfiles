f759f3748e0c1a0697883b8297019951805bd73a
Anders Antila <anders.antila@chas.se>
2015-03-22 23:02:25 +0100

    Fixed awesome installer to enable awesomewm at the gnome login screen.

--- dotfiles/awesome/install.sh
+++ dotfiles/awesome/install.sh
@@ -2,6 +2,8 @@
 
 if test ! $(which awesome)
 then
-  echo "  Installing awesome for you."
-  sudo apt-get install -y awesome
+    echo "  Installing awesome for you."
+    sudo apt-get install -y awesome feh
+    sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /usr/share/xsessions/awesome.desktop
 fi
+

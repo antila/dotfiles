0b518bd1aa56b28986b418480ac5e31d7aab3d70
Anders Antila <anders.antila@chas.se>
2015-03-22 23:01:51 +0100

    Don't run installer as root and added system stow support.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ set -e
 echo ''
 
 info () {
-  printf "  [ \033[00;34m..\033[0m ] $1"
+  printf "  [ \033[00;34m..\033[0m ] $1\n"
 }
 
 user () {
@@ -23,34 +23,43 @@ success () {
 
 fail () {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
+  echo "\n"
   exit
 }
 
 install_dotfiles () {
-  info 'installing dotfiles'
+  info 'Installing dotfiles:'
 
-  for src in $(find "$DOTFILES_ROOT/" -maxdepth 2 -name 'stow')
+  for src in $(find "$DOTFILES_ROOT/" -maxdepth 2 -path ./system -prune -o -name 'stow')
   do
     stow --dir="$src/.." --target=$HOME stow
+    info " - Checking: $src"
   done
+
+  sudo stow --dir="$DOTFILES_ROOT/system/" --target=/ stow
 }
 
 run_installers () {
   # find the installers and run them iteratively
-  info 'Installing stuff...'
+  info 'Installing stuff:'
   find "$DOTFILES_ROOT/" -name install.sh | while read installer ; do sh -c "${installer}" ; done
 }
 
 install_aptitude_stuff () {
   # find the installers and run them iteratively
-  info 'Installing apt stuff...'
+  info 'Installing apt stuff:'
   #sudo apt-get install $(grep -vE "^\s*#" .auto-install  | tr "\n" " ")
 }
 
-run_installers
-install_dotfiles
-#install_aptitude_stuff
+if [ "$EUID" -ne 0 ]; then
+    run_installers
+    install_dotfiles
+    #install_aptitude_stuff
+    echo ''
+    success '  All installed!'
+    exit
+else
+    fail "Don't run this as root."
+    exit
+fi
 
-echo ''
-echo '  All installed!'

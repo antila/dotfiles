6f4f2282fc27732f7795264c356e8da1dc691afe
Anders Antila <anders.antila@chas.se>
2015-06-11 16:20:57 +0200

    New version of Sublime.

--- dotfiles/sublime-text-3/install.sh
+++ dotfiles/sublime-text-3/install.sh
@@ -4,7 +4,7 @@ if test ! $(which subl)
 then
   echo "  Installing zsh for you."
   cd /tmp
-  wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3065_amd64.deb
+  wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
   sudo dpkg -i sublime-text_build-3065_amd64.deb
   cd -
 fi

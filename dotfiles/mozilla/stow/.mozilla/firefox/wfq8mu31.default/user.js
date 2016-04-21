2bdb75f64a82f110aa95a0f8f80f9b50c577df6f
Anders Antila <anders.antila@chas.se>
2016-04-21 19:25:03 +0200

    Add firefox preferences

---
+++ dotfiles/mozilla/stow/.mozilla/firefox/wfq8mu31.default/user.js
@@ -0,0 +1,17 @@
+// //
+// network.http.sendRefererHeader
+//
+//
+// 1
+//
+//
+// Only send HTTP referer header for links clicked on, not for images/media/etc
+//
+// ui.allow_platform_file_picker
+//
+//
+// false Don't use the horrid GTK file picker
+
+// Dark theme
+user_pref("browser.devedition.theme.enabled", true);
+user_pref("devtools.theme", "dark");

a4aa67d8f1ca769b38835b2f0c195fe4b0b45b00
Anders Antila <anders.antila@chas.se>
2016-04-21 19:46:04 +0200

    Need to clean this up some day

--- work.sh
+++ work.sh
@@ -1,22 +1,21 @@
-### Docker ############################################################
-if test ! $(which docker)
-then
-    echo "Installing docker"
-    echo "deb http://http.debian.net/debian jessie-backports main" | sudo tee /etc/apt/sources.list.d/docker.list
-    sudo apt-get update
-    sudo aptitude -t jessie-backports install docker.io
-
-    sudo docker login docker.chas.se
-    sudo docker pull docker.chas.se/oden
-    # sudo docker run --name=oden-dev -d -t -p 443:443 docker.chas.se/oden
-fi
-
-
-### SmartCard ##########################################################
+# SmartCard ##########################################################
 
 # Grab new one
-gpg2 --recv-keys 0A361939
-gpgkey2ssh 700C8678 >> /home/aantila/.ssh/authorized_keys
+# gpg2 --recv-keys 0A361939
+# gpgkey2ssh 700C8678 >> /home/aantila/.ssh/authorized_keys
 
 # ... and add a comment
-sed -i.bak s/COMMENT/"anders@antila.se-yubikey"/g /home/aantila/.ssh/authorized_keys
+# sed -i.bak s/COMMENT/"anders@antila.se-yubikey"/g /home/aantila/.ssh/authorized_keys
+
+# Video
+# sudo apt-get install nvidia-detect nvidia-driver
+
+# YOLO
+# sudo sed -i -e 's/ \(stable\|jessie\)/ testing/ig' /etc/apt/sources.list
+# sudo sed -i -e 's/ \(testing \)/ testing contrib non-free/ig' /etc/apt/sources.list
+# apt-get update
+# apt-get --download-only dist-upgrade
+# apt-get dist-upgrade
+
+# xset dpms force off
+# chsh -s /bin/zsh

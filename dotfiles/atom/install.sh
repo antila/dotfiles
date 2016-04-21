2ee949ac76fa138f6c6188295f73ebf41ec3e824
Anders Antila <anders.antila@chas.se>
2016-04-21 19:27:45 +0200

    Update atom installer

--- dotfiles/atom/install.sh
+++ dotfiles/atom/install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Grab latest atom, also used as update
-echo "Downloading latest version of Atom"
-wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
-wget -q $(awk -F '[<>]' '/href=".*atom-amd64.deb/ {match($0,"href=\"(.*.deb)\"",a); print "https://github.com/" a[1]} ' /tmp/latest) -O /tmp/atom-amd64.deb
-sudo dpkg -i /tmp/atom-amd64.deb
+if test ! $(which atom)
+then
+    # Grab latest atom, also used as update
+    echo "Downloading latest version of Atom"
+    wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
+    wget -q $(awk -F '[<>]' '/href=".*atom-amd64.deb/ {match($0,"href=\"(.*.deb)\"",a); print "https://github.com/" a[1]} ' /tmp/latest) -O /tmp/atom-amd64.deb
+    sudo dpkg -i /tmp/atom-amd64.deb
+fi

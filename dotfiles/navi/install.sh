d8d68619b5da75bfe9419306b80e8d7d299fed93
Anders Antila <anders.antila@chas.se>
2022-04-06 18:03:53 +0200

    fix navi installation

--- dotfiles/navi/install.sh
+++ dotfiles/navi/install.sh
@@ -1,11 +1,9 @@
+#!/bin/sh
 
-
-#!/usr/bin/fish
-
-if ! type -q navi
+if test ! $(which navi)
+then
   # https://github.com/denisidoro/navi
   echo "  Installing navi"
   curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install | bash
-  fish_add_path /home/$USER/.cargo/bin
-
-end
+  fish_add_path /home/$USER/.cargo/bin/navi
+fi

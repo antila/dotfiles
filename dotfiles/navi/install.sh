66db354f3d10e23979a2dbd12cbd932b7e5aacb1
Anders Antila <anders.antila@gmail.com>
2023-02-01 15:35:05 +0000

    fix: navi path

--- dotfiles/navi/install.sh
+++ dotfiles/navi/install.sh
@@ -5,5 +5,4 @@ then
   # https://github.com/denisidoro/navi
   echo "  Installing navi"
   curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install | bash
-  fish_add_path /home/$USER/.cargo/bin/navi
 fi

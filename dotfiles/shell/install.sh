2cc451c9d54a7d4a0937a9ca9461294aefb78ec7
Anders Antila <anders.antila@chas.se>
2024-02-23 00:36:40 +0100

    fix: clean up shell config

---
+++ dotfiles/shell/install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if test ! $(which zsh)
+then
+  echo "  Installing zsh"
+  sudo apt-get install -y zsh
+
+  # Set as shell
+  chsh -s $(which zsh)
+
+  # Download presto
+  # https://github.com/sorin-ionescu/prezto
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+
+if test ! $(which nvm)
+then
+  echo "  Installing nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+fi
+
+if test ! $(which exa)
+then
+  echo "  Installing exa"
+  sudo apt-get install -y exa
+fi
+
+if test ! $(which neovim)
+then
+  echo "  Installing neovim"
+  sudo apt-get install -y neovim
+fi
+
+if test ! $(which direnv)
+then
+  echo "  Installing direnv"
+  sudo apt-get install -y direnv
+fi
+
+if test ! $(which bat)
+then
+  echo "  Installing bat"
+
+  # nicer cat
+  sudo apt-get install -y bat
+  mkdir -p ~/.local/bin
+  ln -s /usr/bin/batcat ~/.local/bin/bat
+fi
--- dotfiles/shell/stow/.zshrc
+++ dotfiles/shell/stow/.zshrc
@@ -19,12 +19,12 @@ eval "$(direnv hook zsh)"
 # Set window title to command just before running it.
 preexec() {
     FOLDER=${PWD/\/home\/anders/"~"}
-    printf "\x1b]0;%s\x07" "$FOLDER/$1"; 
+    printf "\x1b]0;%s\x07" "$FOLDER/$1";
 }
 # Set window title to current working directory after returning from a command.
 precmd() {
     FOLDER=${PWD/\/home\/anders/"~"}
-    printf "\x1b]0;%s\x07" "$FOLDER" 
+    printf "\x1b]0;%s\x07" "$FOLDER"
 }
 
 PATH=$PATH:~/.local/bin/bat

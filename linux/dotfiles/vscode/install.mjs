5f0e281ec3662e08622582ca573b20ffc65fd77b
Anders Antila <anders@antila.se>
2026-06-17 13:34:59 +0200

    fix: don't install vscode

--- linux/dotfiles/vscode/install.mjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zx
-
-import { commandExists } from '../../../common/functions.mjs';
-
-if (!(await commandExists('code'))) {
-  console.log('  Installing Visual Studio Code');
-  await $`sudo snap install code --classic`;
-}
